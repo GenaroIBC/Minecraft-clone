10f8558498f439a614e7c2a5d65aefc8d143297e
genaroibc <bonavitaismael@gmail.com>
2022-11-17 20:48:59 -0300

    [FEAT]: render components

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+import { Ground } from "./components/Ground";
+import { FirstPointOfView } from "./components/FirstPointOfView";
+import { Player } from "./components/Player";
+import Cubes from "./components/Cubes";
+import { useWorldReducer } from "./reducers/worldReducer/worldReducer";
+
+export default function App() {
+  const { addCube, worldState, removeCube } = useWorldReducer();
+
+  return (
+    <>
+      <Canvas>
+        <Sky sunPosition={[1, 0, 1]} />
+        <ambientLight intensity={0.5} />
+        <FirstPointOfView />
+        <Physics>
+          <Cubes removeCube={removeCube} cubes={worldState.cubes} />
+          <Player />
+          <Ground addCube={addCube} />
+        </Physics>
+      </Canvas>
+      <div className="player-pointer">+</div>
+    </>
+  );
+}
